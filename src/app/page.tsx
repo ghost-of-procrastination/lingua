4808ccb9d4c44a2c7210fbb41a5e97ab10dab2c4
Vladyslav <ghostofprocrastination@gmail.com>
2025-02-28 02:05:20 +0200

    Initialize app

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+	return (
+		<div>
+			<Button size="lg" variant="destructive" className="cursor-pointer">
+				we
+			</Button>
+		</div>
+	)
+}
+
+export default Home
